42ae5265f744c3d6fd21f4a76a2889e9e1e19f20
Noax89 <moshoodqudus89@gmail.com>
2024-12-29 12:07:55 +0100

    add new changes

--- src/assets/Components/LoginSignin/TokenAuth.jsx
+++ src/assets/Components/LoginSignin/TokenAuth.jsx
@@ -1,15 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 export const TokenAuth = () => {
     const navigate = useNavigate();
 
     const [tokenCount, setTokenCount]= useState(59);
-    function handleTokenCount(){
-    setInterval(()=>{
-        setTokenCount(tokenCount -1)
-    }, 1000)
-    }
+
+useEffect(()=>{
+  setTimeout(()=>{
+    setTokenCount(tokenCount=> tokenCount -1)
+  }, 1000)
+})
   return (
     <div className='loginWrapper'>
     <h3>Verify Token</h3>
@@ -22,7 +23,7 @@ export const TokenAuth = () => {
     <input type='text' maxLength={1} minLength={1} />
     </div> 
     <button className='LoginBtn' onClick={()=>navigate('/Login')}>Next</button> 
-    Token will be expired in {()=>handleTokenCount()} mins. <a href='#'>Resend</a>
+    Token will be expired in {tokenCount} Secs. <a href='#'>Resend</a>
 
     </form>
   </div>
